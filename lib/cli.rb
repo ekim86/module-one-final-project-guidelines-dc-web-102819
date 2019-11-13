fbd7c73cec5310bcd3e9731a4b26309fafc0ad1f
github username <github email address>
2019-11-13 09:37:02 -0500

    name change and ingredient

--- lib/cli.rb
+++ lib/cli.rb
@@ -16,10 +16,9 @@ end
 def help
   help = <<-HELP
     
-  Please choose one of the following:
+  Please type one of the following:
     - Recipes : displays all the recipes
     - Ingredients : displays a list of ingredients to choose from
-    - Blank :
     - Exit : exits the app
     HELP
   puts help
@@ -29,9 +28,9 @@ end
 def options
   options = <<-OPTIONS
   
-  Please choose one of the following:
+  Please type one of the following:
   - Calories : displays the calories for the recipe
-  - Serving : displays the ingredients needed for the recipe
+  - Servings : displays the ingredients needed for the recipe
   - Both : Calories and serving size
   - Main Menu : go back to main menu
   OPTIONS
@@ -60,7 +59,7 @@ end
 
 def recipe_submenu
   list_recipes
-  puts "Please select a recipe to see the directions:"
+  puts "Please type the number of the recipe to see the directions:"
     user_input = gets.chomp
     if (user_input.to_i) <= Recipe.all.count
       chosen_recipe = Recipe.all[user_input.to_i - 1]
@@ -76,14 +75,14 @@ def recipe_detail_menu(recipe)
   command = gets.downcase.strip
 case command
   when 'calories'
-    puts recipe.calories
+    puts "Calories #{recipe.calories}"
     help
   when 'servings'
-    puts recipe.servings
+    puts "Servings #{recipe.servings}"
     help
   when 'both'
-    puts recipe.calories
-    puts recipe.servings
+    puts "Calories #{recipe.calories}"
+    puts "Servings #{recipe.servings}"
     help
   when 'main menu'
     help
@@ -97,29 +96,16 @@ def list_recipes
   puts "#{index+1}. #{recipe.name}"}
 end
 
-
-# def calories
-#   Recipe.all.find do |recipe|
-#   puts recipe.calories
-#   end
-# end
-
-
 def ingredient_submenu
   list_ingredients
   puts "Please type the ingredient to find recipes:"
     user_input = gets.chomp
-    # if (user_input) IngredientRecipe.all.count
       ingredient_search = IngredientRecipe.all.select do |ingredientrecipe|
         ingredientrecipe.ingredient.name.downcase == user_input.downcase
       end
-  # binding.pry
       ingredient_search.each do |ingredientsearch|
         puts ingredientsearch.recipe.name
       end
-    # else
-      # puts "Invalid input, please try again."
-    # end
     help
 end
 # if ingredient doesnt exist conditional
